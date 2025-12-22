0e7d605b3efad48edf4761835fa31f16823b3714
Dmitry <dmitrijladov396@gmail.com>
2025-12-22 19:36:42 +0300

    Data functions added.

--- crawler.js
+++ crawler.js
@@ -47,18 +47,46 @@ async gather_data(){
 }
 
 
+class DataTools {
+
+  static Validate(list){
+    if (list.length == 0) {console.log("Empty list given!"); throw new Error("Empty list on data pipeline!")}  
+
+  }
 
 
+  static pruneOnEmptyData(list){
+    this.Validate(list)
+         for(let i = list.length-1;i>=0;--i){
+        if (list[i].href == "" || list[i].header == ""){
+            list.splice(i,1)
+        }
+    }
+     
+  }
 
 
+  static pruneOnFewWords(list){
+    this.Validate(list)
+     
+     for(let i = list.length-1;i>=0;--i){
+        if (list[i].header.split(" ").length < 3){
+            list.splice(i,1)
+        }
+  }
+}
+
+}
 
 
 
 
 async function main(){
 const br = new Browser(browser_addr)
-await br.go_to(specs.yahoo.BASE_URL)
+await br.go_to(specs.reuters.BASE_URL)
 await br.gather_data()
+DataTools.pruneOnEmptyData(br.data)
+DataTools.pruneOnFewWords(br.data)
 console.log(br.data)
 }
 
