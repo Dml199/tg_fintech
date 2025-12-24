1f95f6c4eae4b2658dcf810cf8b60b962fdedb20
Dmitry <dmitrijladov396@gmail.com>
2025-12-24 20:25:40 +0300

    testing git log_1.01: Change logic to puppeteer specific in recursive node depth traversal

--- tools.js
+++ tools.js
@@ -30,4 +30,29 @@ static async purifyData(list){
   this.pruneOnEmptyData(list);
   this.pruneOnFewWords(list);
 }
+
+
+
 }
+
+
+
+export class DomLogicHandler{
+
+  constructor(){
+
+  }
+
+ checkCurrentNode(node){
+  data_arr = []
+  
+  if(node.children.length >= 8){
+    for (let i = 0; i< elem_arr.length; ++i){
+         data_arr.push(elem_arr[i].innerText)
+    } return data_arr
+  }
+  else {for (child of node.children){checkCurrentNode(child)}}
+}
+
+}
+
